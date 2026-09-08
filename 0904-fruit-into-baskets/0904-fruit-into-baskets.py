1bb66c47293127e7395fec8ac69b83e1a5b0c08a
Shreyaa Dani <59827804+shreyaadani@users.noreply.github.com>
2026-09-08 15:21:21 -0700

    Time: 187 ms (55.97%), Space: 26 MB (34.91%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        basket = {}
+        res = 0
+        l,r = 0,0
+
+        for r in range(len(fruits)):
+            basket[fruits[r]] = basket.get(fruits[r],0) + 1
+            while len(basket) > 2:
+                basket[fruits[l]] -=1
+                if basket[fruits[l]] == 0:
+                    del basket[fruits[l]]
+                l+=1
+
+            res = max(res,r-l+1)
+
+        return res            
+
+        
