c7609d69bd18107650791bccae45bd9b28c27fbd
Shreyaa Dani <59827804+shreyaadani@users.noreply.github.com>
2026-09-17 18:18:45 -0700

    Time: 3 ms (38.43%), Space: 19.3 MB (65.35%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,17 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+
+        for p in path.split("/"):
+            if p == "..":
+                if stack:
+                    stack.pop()
+            elif p == "." or not p:
+                    continue
+            else:
+                stack.append(p)
+
+        final_str ="/"+ "/".join(stack)
+
+        return final_str                    
+        
